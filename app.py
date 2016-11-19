f1bc1dbfd7c03059e7f19ad8a9e75305dae1822a
RobbieDeegan <rdeegan10@gmail.com>
2016-11-19 16:19:59 +0000

    Color changes

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def root():
     return app.send_static_file('index.html')
 
 @app.route("/password", methods=["GET", "POST"])
-def passwordGen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+
+# Code for generatign randomlly found from: http://www.practicepython.org/solution/2014/06/06/16-password-generator-solutions.html
+def passwordGen(size = 8, chars = string.ascii_letters + string.digits + string.punctuation):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 
