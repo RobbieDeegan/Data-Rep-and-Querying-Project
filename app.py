f87e1fa6dc16966d2ec1e63b3a7d194f26f065e4
RobbieDeegan <rdeegan10@gmail.com>
2016-11-21 15:47:57 +0000

    small fixes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import flask as fl
 
-app = fl.Flask(__name__)
-
 import string
 import random
 import json
+
+app = fl.Flask(__name__)
+
+#size = (fl.request.values["userinput"])
 
 @app.route("/")
 def root():
@@ -12,7 +14,7 @@ def root():
 
 @app.route("/password", methods=["GET", "POST"])
 
-# Code for generating randomlly found from: http://www.practicepython.org/solution/2014/06/06/16-password-generator-solutions.html
+# Code for generating randomly found from: http://www.practicepython.org/solution/2014/06/06/16-password-generator-solutions.html
 def passwordGen(size = 10, chars = string.ascii_letters + string.digits + string.punctuation):
 	return ''.join(random.choice(chars) for _ in range(size))
 
